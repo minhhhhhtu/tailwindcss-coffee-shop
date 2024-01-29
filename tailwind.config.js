778de5a516b75cf3b0e7d08e3ce3039ea01c99a7
minhtu <leminhtu17062003@gmail.com>
2024-01-30 02:08:19 +0700

    Add featured mugs - product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,12 @@ module.exports = {
         },    
       },
       keyframes: {
-        slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
-        }
+        slideDown: {'0%': { transform: 'translateY(-100%)' }, '100%': { transform: 'translateY(0)' },}, 
+        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 }, },
       }, 
       animation: {
         slideDown: 'slideDown 0.4s ease-in-out',
+        fadeIn: 'fadeIn 0.4s ease-in-out',
       },
     },
   },
