630c82d4d37061178bd0bbdab8954d4969599caf
minhtu <leminhtu17062003@gmail.com>
2024-01-30 23:21:31 +0700

    Add parallax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,36 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"], 
+  content: ["./public/**/*.{html,js}"],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {
-      screens: {
-        // 'xs': '480px',
-      },
       fontFamily: {
-        Karla: ['Karla', 'sans-serif']
+        Karla: ["Karla", "sans-serif"],
       },
       colors: {
-        'coffee': {
-          50: '#E8D6D0',
-          200: '#C89F94',
-          400: '#A25F4B',
-          600: '#744838',
-        },    
+        coffee: {
+          50: "#E8D6D0",
+          200: "#C89F94",
+          400: "#A25F4B",
+          600: "#744838",
+        },
       },
       keyframes: {
-        slideDown: {'0%': { transform: 'translateY(-100%)' }, '100%': { transform: 'translateY(0)' },}, 
-        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 }, },
-      }, 
+        slideDown: {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 } },
+      },
       animation: {
-        slideDown: 'slideDown 0.4s ease-in-out',
-        fadeIn: 'fadeIn 0.4s ease-in-out',
+        slideDown: "slideDown 0.4s ease-in-out",
+        fadeIn: "fadeIn 0.4s ease-in-out",
       },
     },
   },
   plugins: [],
-}
-
+};
